4b497a5d8a8c8d042395270aab0f32a2c8e608a5
Carlos <carlos.1.1.8.u@hotmail.com>
2018-09-28 20:13:57 -0500

    message

--- index.php
+++ index.php
@@ -1,6 +1,32 @@
 <?php
 
-   echo "HOLAAAAAAA";
+
+require("phpMQTT.php");
+
+
+echo "<h1> MQTT </h1>";
+
+$host = "tcp://m15.cloudmqtt.com:15440";
+$port = "15440";
+$username = "gxcheyfv";
+$password = "X9BYHngj4usD";
+$message = "";
+
+$mqtt = new phpMQTT($host, $port, "carlos");
+
+echo "Iniciando";
+if($mqtt->connect(true,NULL,$username,$password)){
+
+ echo "Conectado";
+ $mqtt->publish("bluerhinos/phpMQTT/examples/publishtest","Hello World! at ".date("r"),0);
+ $mqtt->close();
+
+
+}
+else{
+
+ echo "No conecto;";
+}
 
 
 ?>
